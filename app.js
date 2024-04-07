5eecb46b0d970221bd847d3572c86a8735faddb2
carlinutd <160259519+carlinutd@users.noreply.github.com>
2024-04-07 15:38:24 -0300

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector(".nav");
-if(hamburger && nav) {
-    hamburger.addEventListener("click", () => nav.classList.toggle("active"));
-}
-
-
